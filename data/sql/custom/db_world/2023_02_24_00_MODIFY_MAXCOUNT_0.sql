ad9f50aa87499dc4ba067504657df151d7880be8
周剑钧 <hzzhoujianjun@corp.netease.com>
2023-02-24 21:40:56 +0800

    Fixed(sql): 移除70级戒指的唯一属性

---
+++ data/sql/custom/db_world/2023_02_24_00_MODIFY_MAXCOUNT_0.sql
@@ -0,0 +1,7 @@
+UPDATE `item_template` SET `maxcount` = 0 WHERE `entry` IN 
+(
+  37927, -- item=37927/护卫者的镇压指环
+  35129, -- item=35129/护卫者的统御指环
+  35130, -- item=35130/护卫者的拯救指环
+  35131, -- item=35131/护卫者的胜利指环
+);
