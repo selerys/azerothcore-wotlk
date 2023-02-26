d9017ff369704c470c6b28ef5b819dafdd94b4bc
Michaud Chow <selerys@163.com>
2023-02-26 21:30:49 +0800

    Fixed(super_hearthstone): 尝试停止施法

--- scripts/super_hearthstone.lua
+++ scripts/super_hearthstone.lua
@@ -8,6 +8,7 @@ local GOSSIP_EVENT_ON_SELECT = 2
 --
 local function OnCustomItemCallingGossip(event, player, item)
     player:SendBroadcastMessage(item:GetName())
+    return false
 end
 
 -- 注册物品的hello事件
